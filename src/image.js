d8073ca1d08486cb15b307339dd0a3e3580cd3da
boskikir <rymwmwx@gmail.com>
2025-11-27 13:47:54 +0700

    Add files via upload

---
+++ src/image.js
@@ -0,0 +1,60 @@
+// src/image.js
+// Central image list for plugins to read.
+// Add more keys later, e.g. 'random_cwe_anime': [ ...urls ]
+
+module.exports = {
+  // key name used by plugin: random_blue_arcive
+  random_waguri: [
+    // example urls (ganti dengan url nyata dari upstream kamu)
+    "https://i.pinimg.com/originals/86/93/cb/8693cb0b92b5e79468fbac0fa96242cd.jpg",
+"https://i.pinimg.com/originals/f6/63/31/f66331353776d66056498a4bef50b1bb.jpg",
+"https://i.pinimg.com/originals/dc/9f/98/dc9f98acf86e7bd07a00836f1ad99aad.jpg",
+"https://i.pinimg.com/originals/4d/be/5a/4dbe5ae05cdb63bcfec2f2842c129b6c.png",
+"https://i.pinimg.com/originals/d9/6c/fc/d96cfcacfabe37ee103acb61a1f20a72.webp",
+"https://i.pinimg.com/originals/a6/c0/33/a6c033a4083f61f38e30608247473965.jpg",
+"https://i.pinimg.com/originals/38/05/42/38054213794fe06971c754b4a90d5a6a.png",
+"https://i.pinimg.com/originals/77/5e/3e/775e3ea935741b7885ad757c648bca7d.jpg",
+"https://i.pinimg.com/originals/88/2d/91/882d915918d7aa28c58a926e4ed3177b.jpg",
+"https://i.pinimg.com/originals/06/2a/53/062a53f2e25317bae0bf5b80bb6f98df.png",
+"https://i.pinimg.com/originals/a0/27/77/a02777958e8c9b60c7768f6506fe89a5.jpg",
+"https://i.pinimg.com/originals/e4/35/18/e435188a2a91418bc693efe2f7573a7d.webp",
+"https://i.pinimg.com/originals/b5/3b/e8/b53be87abec032413f4e91983f7c41fc.jpg",
+"https://i.pinimg.com/originals/97/c0/f0/97c0f015c7be3765f0ab4173a75905ea.jpg",
+"https://i.pinimg.com/originals/67/b3/ac/67b3acd067870b8dea7fd68386828d70.png",
+"https://i.pinimg.com/originals/a7/6d/d8/a76dd8f238cb020a3932fb2ec583ca37.jpg"
+    // tambahkan sebanyak yang kamu mau
+  ],
+
+  // you can add other lists
+  random_blue_archive: [
+    "https://files.catbox.moe/54dtrl.jpeg",
+"https://files.catbox.moe/xfv38q.jpeg",
+"https://files.catbox.moe/9hwglh.jpeg",
+"https://files.catbox.moe/rbocth.jpeg",
+"https://files.catbox.moe/sxdcdk.jpeg",
+"https://files.catbox.moe/5qmaup.jpeg",
+"https://files.catbox.moe/cwlum1.jpeg",
+"https://files.catbox.moe/z4xfst.jpeg",
+"https://files.catbox.moe/xgz6ln.jpeg",
+"https://files.catbox.moe/4g8hn7.jpeg",
+"https://files.catbox.moe/kn1aaj.jpeg",
+"https://files.catbox.moe/da2r0s.jpeg",
+"https://files.catbox.moe/rpf820.jpeg",
+"https://files.catbox.moe/opyzq8.jpeg",
+"https://files.catbox.moe/vq8hgh.jpeg",
+"https://files.catbox.moe/4dn1qe.jpeg",
+"https://files.catbox.moe/p1ki20.jpeg",
+"https://files.catbox.moe/kfx1da.jpeg",
+"https://files.catbox.moe/jxl634.jpeg",
+"https://files.catbox.moe/q856h2.jpeg",
+"https://files.catbox.moe/a7lvd0.jpeg",
+"https://files.catbox.moe/zjf7ix.jpeg",
+"https://files.catbox.moe/6zoo2v.jpeg",
+"https://files.catbox.moe/e1fu11.jpeg",
+"https://files.catbox.moe/7btnzt.jpeg",
+"https://files.catbox.moe/wgw5fe.jpeg",
+"https://files.catbox.moe/y7u839.jpeg",
+"https://files.catbox.moe/rdb60n.jpeg",
+"https://files.catbox.moe/2o08kp.jpeg"
+  ]
+};
